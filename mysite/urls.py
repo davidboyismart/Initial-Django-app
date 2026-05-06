fefb70c609f41544d0396ff88f924ee4175e8f1f
david_abdullah@tutamail.com <david_abdullah@tutamail.com>
2026-05-06 20:41:20 +0300

    Update home page to confirm deployment

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,8 +7,10 @@ def home(request):
             <head><title>Document</title></head>
             <body>
                 <h1><title>Azure Django Lab</title></h1> 
+                <p>Hello from azure!</p>
                 <p>This Django app is running on Azure app Service.</p>
                 <p>Deployed automatically via github actions CI/CD pipeline.</p>
+                <p><strong>pipeline is working!</strong></p>
             </body>
             </html>
 """)
