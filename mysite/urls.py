8b97ffadd56af8827fce9fb85e2331256740f6c7
david_abdullah@tutamail.com <david_abdullah@tutamail.com>
2026-05-06 19:31:35 +0300

    Initial Django app

---
+++ mysite/urls.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.urls import path
+
+
+def home(request):
+    return HttpResponse("""<html>
+            <head><title>Document</title></head>
+            <body>
+                <h1><title>Azure Django Lab</title></h1> 
+                <p>This Django app is running on Azure app Service.</p>
+                <p>Deployed automatically via github actions CI/CD pipeline.</p>
+            </body>
+            </html>
+""")
+
+urlpatterns = [
+    path('', home),
+]
